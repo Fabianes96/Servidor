406d0b131355ed627b16cd295096e98de65fba77
Fabian <fabianes1996@gmail.com>
2018-08-29 19:57:15 -0500

    Versión beta

--- Servidor/Form1.Designer.cs
+++ Servidor/Form1.Designer.cs
@@ -35,7 +35,10 @@
             this.txt_Text = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.rich_Text = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScoreFinal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -107,21 +110,47 @@
             this.rich_Text.TabIndex = 5;
             this.rich_Text.Text = "";
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(373, 247);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.Location = new System.Drawing.Point(356, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 48);
+            this.label1.TabIndex = 6;
+            // 
+            // lblScoreFinal
+            // 
+            this.lblScoreFinal.Location = new System.Drawing.Point(315, 241);
+            this.lblScoreFinal.Name = "lblScoreFinal";
+            this.lblScoreFinal.Size = new System.Drawing.Size(35, 48);
+            this.lblScoreFinal.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(443, 292);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblScoreFinal);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.rich_Text);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txt_Text);
@@ -159,7 +188,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScoreFinal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Servidor/Form1.cs
+++ Servidor/Form1.cs
@@ -19,28 +19,32 @@ namespace Servidor
         private byte[] _buffer = new byte[1024];
         public List<SocketT2h> __ClientSockets { get; set; }
         List<string> _names = new List<string>();
+        public string puntuacion;
+        private int contador =0;
+        private int maximo;
+        //string ip = "192.168.0.14";
+        string dir;
         private Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         public Form1()
         {
             InitializeComponent();
             __ClientSockets = new List<SocketT2h>();
             CheckForIllegalCrossThreadCalls = false;
-
-        }
-        public void oajd8() { }
+        }       
 
         public void escuchar()
         {
             try
             {
+                dir = Microsoft.VisualBasic.Interaction.InputBox(
+                "Escriba la dirección ip", "Texto del dialogo", "");
                 //Socket servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                //IPEndPoint direccion = new IPEndPoint(IPAddress.Parse(ip), puerto);
-                //servidor.Bind(direccion);
+                IPEndPoint direccion = new IPEndPoint(IPAddress.Parse(dir), puerto);
+                _serverSocket.Bind(direccion);
                 lb_stt.Text = "Preparando servidor";
-                _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+                //_serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
                 _serverSocket.Listen(1);
                 _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
-
             }
             catch (Exception error)
             {
@@ -55,10 +59,10 @@ namespace Servidor
 
             lb_stt.Text = "Numero de clientes conectados: " + __ClientSockets.Count.ToString();
             lb_stt.Text = "Cliente conectado. . .";
-            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(RecibeMensaje), socket);
             _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
         }
-        private void ReceiveCallback(IAsyncResult ar)
+        private void RecibeMensaje(IAsyncResult ar)
         {
             Socket socket = (Socket)ar.AsyncState;
             if (socket.Connected)
@@ -75,8 +79,20 @@ namespace Servidor
                     {
                         if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
                         {
+                            
+                            if (label1.Text.Contains(__ClientSockets[i]._Name))
+                            {
+                                label1.Text= label1.Text.Replace(__ClientSockets[i]._Name, " ");
+                            }
+                           // lb_stt.Text = "Numero de clientes conectados: " + __ClientSockets.Count.ToString();
+                            contador--;
+                            label3.Text = contador.ToString();
+                            Sendata(socket, label1.Text);
+                            enviarInfo();
+                            //label4.Text = __ClientSockets[i]._Name;
                             __ClientSockets.RemoveAt(i);
                             lb_stt.Text = "Numero de clientes conectados: " + __ClientSockets.Count.ToString();
+                            
                         }
                     }
                     // Eliminar en la lista
@@ -87,18 +103,37 @@ namespace Servidor
                     byte[] dataBuf = new byte[received];
                     Array.Copy(_buffer, dataBuf, received);
                     string text = Encoding.ASCII.GetString(dataBuf);
+                    string aux;
                     lb_stt.Text = "Texto recibido: " + text;
+                    if (text.Contains(": "))
+                    {
+                        aux = text.Substring(text.IndexOf(" ")+1);
+                        label2.Text = aux;
+                        if (maximo < int.Parse(aux))
+                        {
+                            maximo = int.Parse(aux);
+                        }                        
+                        lblScoreFinal.Text = lblScoreFinal.Text + text + Environment.NewLine;
+                        contador++;
+                        label3.Text = contador.ToString();
+                        Sendata(socket ,lblScoreFinal.Text);
+                        enviarPuntos();
+                        return;
+                    }                  
 
                     string reponse = string.Empty;         
                     for (int i = 0; i < __ClientSockets.Count; i++)
                     {
                         if (socket.RemoteEndPoint.ToString().Equals(__ClientSockets[i]._Socket.RemoteEndPoint.ToString()))
                         {
-                            rich_Text.AppendText("\n" + __ClientSockets[i]._Name + ": " + text);
+                            __ClientSockets[i]._Name = text;
+                            rich_Text.AppendText(":" + text+"\n");
                         }
-                    }                    
-                    reponse = "Servidor recibido" + text;
-                    Sendata(socket, reponse);
+                    }
+                    label1.Text = label1.Text + text + Environment.NewLine;
+                    //reponse = "Usuarios conectados";
+                    Sendata(socket, label1.Text);
+                    enviarInfo();
                 }
                 else
                 {
@@ -106,19 +141,37 @@ namespace Servidor
                     {
                         if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
                         {
+                            
+                            if (label1.Text.Contains(__ClientSockets[i]._Name))
+                            {
+                                label1.Text= label1.Text.Replace(__ClientSockets[i]._Name, " ");
+                            }                
+                            contador--;
+                            label3.Text = contador.ToString();
+                            Sendata(socket, label1.Text);
+                            enviarInfo();
                             __ClientSockets.RemoveAt(i);
-                            lb_stt.Text = "Clientes conectados: " + __ClientSockets.Count.ToString();
+                            lb_stt.Text = "Numero de clientes conectados: " + __ClientSockets.Count.ToString();
                         }
                     }
                 }
             }
-            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(RecibeMensaje), socket);
         }
         void Sendata(Socket socket, string noidung)
         {
-            byte[] data = Encoding.ASCII.GetBytes(noidung);
-            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
-            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+                if (socket.Connected)
+                {
+                    byte[] data = Encoding.ASCII.GetBytes(noidung);
+                    socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+                    _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+                }
+                else
+                {                    
+                    return;
+                }    
+            
+
         }
         private void SendCallback(IAsyncResult AR)
         {
@@ -132,23 +185,59 @@ namespace Servidor
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        {          
+           
             for (int i = 0; i < lista_clientes.SelectedItems.Count; i++)
-            {
+            {                
                 string t = lista_clientes.SelectedItems[i].ToString();
                 for (int j = 0; j < __ClientSockets.Count; j++)
                 {
                     {
-                        Sendata(__ClientSockets[j]._Socket, txt_Text.Text);
+                        Sendata(__ClientSockets[j]._Socket, label1.Text);
                     }
                 }
             }
             rich_Text.AppendText("\nServer: " + txt_Text.Text);
 
         }
+        public void enviarPuntos()
+        {
+            
+            for (int i = 0; i < lista_clientes.Items.Count; i++)
+            {
+                lista_clientes.SetSelected(i, true);
+            }
+            for (int i = 0; i < lista_clientes.SelectedItems.Count; i++)
+            {
+                string t = lista_clientes.SelectedItems[i].ToString();
+                for (int j = 0; j < __ClientSockets.Count; j++)
+                {
+                    {
+                        Sendata(__ClientSockets[j]._Socket, lblScoreFinal.Text);
+                        if (label3.Text == __ClientSockets.Count.ToString())
+                        {
+                            Sendata(__ClientSockets[j]._Socket, label3.Text+"°"+maximo.ToString());
+                        }
+                    }
+                }
+            }
+        }
         public void enviarInfo()
         {
-
+            for (int i = 0; i < lista_clientes.Items.Count; i++)
+            {
+                lista_clientes.SetSelected(i, true);
+            }
+            for (int i = 0; i < lista_clientes.SelectedItems.Count; i++)
+            {
+                string t = lista_clientes.SelectedItems[i].ToString();
+                for (int j = 0; j < __ClientSockets.Count; j++)
+                {
+                    { 
+                        Sendata(__ClientSockets[j]._Socket, label1.Text);
+                    }
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -160,9 +249,10 @@ namespace Servidor
     {
         public Socket _Socket { get; set; }
         public string _Name { get; set; }
+        public int puntos { get; set; }
         public SocketT2h(Socket socket)
         {
-            this._Socket = socket;
+            _Socket = socket;
         }
     }
 }
